f46a597fe96dada7067395f0f7b97704aac8fdc1
khuzaima <khuzaimagoraya786@gmail.com>
2024-10-17 13:19:15 +0500

    updated

--- newproject-demo.js
+++ newproject-demo.js
@@ -5,7 +5,7 @@ xhr.open('GET', requesturl);
 xhr.onreadystatechange=function(){
    console.log(xhr.readyState);
    if(xhr.readyState === 4){
-       const data = JSON.parse(this.response)
+       const data = JSON.parse(this.responseText)
        console.log(data);
    }
 }
